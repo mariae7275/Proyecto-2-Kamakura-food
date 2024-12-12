d725e87b02e90a693add9beeb5f8286021c47841
AlejandroAriasSL <alejandroasilva02@gmail>
2024-12-12 15:35:36 +0100

    "Print filters"

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,15 @@
 //DEBE imprimir en pantalla la información de filtros.
+import {filters} from "../assets/data/data.js"
 
+function addFilter () {
+    const filtersContainer = document.querySelector(".filters-container")
+    filtersContainer.innerHTML = "";
+    filters.map(el => {
+        const buttonFilter = document.createElement("button");
+        buttonFilter.textContent = el;
+        filtersContainer.appendChild(botonFilter);
+        buttonFilter.classList.add("filter");
+    })
+}
+addFilter();
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
